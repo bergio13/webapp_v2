7fd3a85b3a55f641a2c693538607ae6fe408de09
Giochelavaipiatti <78697256+bergio13@users.noreply.github.com>
2023-03-11 21:09:05 +0100

    Added dynamic data and json api

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+movies = ['a', 'b']
 
 @app.route('/')
 def hello():
     return render_template('home.html')
+
+@app.route('/lista')
+def lista():
+    return render_template('lista.html', months=months, movies=movies)
+
+@app.route('/api/about')
+def list_about():
+    return jsonify(months)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
