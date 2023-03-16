ad52d7959002858ba41ad84fde82d7a078855b38
Giochelavaipiatti <78697256+bergio13@users.noreply.github.com>
2023-03-16 18:34:48 +0100

    Login, register and porfile routes

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template, jsonify
-from database import load_users_from_db, load_users_from_username
+from flask import Flask, render_template, jsonify, request, flash, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from database import load_users_from_db, load_users_from_username, engine, load_users_from_email, insert_user, get_user_id
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+#class User(db.Model, UserMixin):
+#    __tablename__ = 'users'
+#
+#    id = Column(Integer, primary_key=True, autoincrement=True)
+#    username = Column(String(50), unique=True, nullable=False)
+#    email = Column(String(120), unique=True, nullable=False)
+#    password = Column(String(120), nullable=False)
+#    
+#    def __init__(self, username, email, password):
+#        self.username = username
+#        self.email = email
+#        self.password = password
+#
+##    def __repr__(self):
+##        return f"<User(username='{self.username}', email='{self.email}')>"
+##
    
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', session=session)
 
 @app.route('/lista')
 def lista():
@@ -22,16 +43,90 @@ def show_user_profile(name):
 
 #@app.route('/api/about')
 #def list_about():
-#    return jsonify()
-#
+#    data = get_user_id(1)
+#    user = User(id=data[0]['id'], username=data[0]['username'], email=data[0]['email'], password=data[0]['password'])
+#    return jsonify(data[0])
+##
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+        """Register user"""
+        if request.method == "POST":
+            # Require username
+            email = request.form.get("email")
+            name = request.form.get("username")
+            password = request.form.get("password")
 
-@app.route('/login')
+            # Check if email already exists
+            users = load_users_from_email(email)
+            usernames = load_users_from_username(name)
+            if users:
+                flash('Email already exists', category='error')
+            elif not email or len(email) < 4:
+                flash('Email must be valid', category='error')
+            if usernames:
+                flash('Username already taken', category='error')
+            elif len(name) < 2 :
+                flash('Username must be greater than 1 character', category='error')
+            # Check and validate passwords
+            elif not password:
+                flash("Must provide password", category='error')
+            elif len(password) < 3:
+                flash("Password must be greater than 3 characters", category='error')
+            else:
+            # Create hash of password to insert into the database
+                hash = generate_password_hash(request.form.get("password"), method='sha256')
+
+                insert_user(name, email, password=hash)
+                flash('Account created', category='success')
+
+                return redirect("/")
+
+        return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        
+        email = request.form['email']
+        password = request.form['password']
+        
+        users = load_users_from_email(email)
+        if users != []:
+            if users[0]['email'] == email and users[0]['password'] == password:
+                session['loggedin'] = True
+                session['id'] = users[0]['id']
+                session['email'] = users[0]['email']
+                flash ('Logged in successfully!', category='success')
+                return redirect('/')
+         # If account exists in accounts table in out database
+            else:
+            # Account doesnt exist or username/password incorrect
+                flash ('Incorrect username/password!')
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    return render_template("login.html", session=session)
+
+@app.route('/profile')
+def profile():
+    if 'loggedin' in session:
+        users = get_user_id(session['id'])
+        return render_template('profile.html', user=users[0])
+    return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('email', None)
+   # Redirect to login page
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -26,7 +26,6 @@ def load_users_from_username(name):
     with engine.connect() as conn:
         # define a SQL query with a parameter
         query = text('SELECT * FROM users WHERE username = :username').bindparams(username=name)
-
         # execute the query with a parameter value
         result = conn.execute(query)
         
@@ -36,6 +35,44 @@ def load_users_from_username(name):
             user_dict["id"] = row[0]
             user_dict["username"] = row[1]
             user_dict["email"] = row[2]
+            user_dicts.append(user_dict)
+        return user_dicts
+
+def load_users_from_email(email):
+    with engine.connect() as conn:
+        # define a SQL query with a parameter
+        query = text('SELECT * FROM users WHERE email = :email').bindparams(email=email)
+        # execute the query with a parameter value
+        result = conn.execute(query)
+        
+        user_dicts = []
+        for row in result:
+            user_dict = {}
+            user_dict["id"] = row[0]
+            user_dict["username"] = row[1]
+            user_dict["email"] = row[2]
+            user_dict["password"] = row[3]
+            user_dicts.append(user_dict)
+        return user_dicts
+
+def insert_user(username, email, password):
+    with engine.connect() as conn:
+        query = text('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)').bindparams(username=username, email=email, password=password)
+        conn.execute(query)
+        
+def get_user_id(id):
+    with engine.connect() as conn:
+        # define a SQL query with a parameter
+        query = text('SELECT * FROM users WHERE id = :id').bindparams(id=id)
+        # execute the query with a parameter value
+        result = conn.execute(query)
+        user_dicts = []
+        for row in result:
+            user_dict = {}
+            user_dict["id"] = row[0]
+            user_dict["username"] = row[1]
+            user_dict["email"] = row[2]
+            user_dict["password"] = row[3]
             user_dicts.append(user_dict)
         return user_dicts
         
