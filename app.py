6c4c6415272ca858171b1bfc6f6ed2bc09043ebe
Giochelavaipiatti <78697256+bergio13@users.noreply.github.com>
2026-01-10 11:06:04 +0100

    change limiter

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ csrf = CSRFProtect(app)
 limiter = Limiter(
     key_func=get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["1000 per day", "50 per hour"],
     storage_uri="memory://"
 )
 
